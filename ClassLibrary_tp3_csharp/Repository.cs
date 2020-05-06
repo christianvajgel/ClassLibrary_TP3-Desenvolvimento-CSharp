872c3cdf439bef2b66fefcd1ee56a3914f0a3d9f
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2020-05-06 17:26:03 -0300

    Created method for database operations.

--- ClassLibrary_tp3_csharp/Repository.cs
+++ ClassLibrary_tp3_csharp/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
@@ -16,42 +17,67 @@ namespace ClassLibrary_tp3_csharp
         //public List<Person> People { get; set; }
         ////internal List<Person> People { get => people; set => people = value; }
 
-        private static List<Person> people = new List<Person>();
+        public static List<Person> people = new List<Person>();
 
-        public static string addPerson(Person person) 
+        public static string AddPerson(Person person) 
         {
-            var ok = false;
-            var message = "";
-            foreach (var match in people)
-            {
-                if (match == person)
+            return new Func<String>(() => {
+                if (CheckContactExistence(person)) { return "Person already exists."; }
+                else { return "Person added."; }
+            })();
+        }
+
+        public static IEnumerable<Person> SearchPeople(string term) 
+        {
+            return people.Where(person => person.FirstName.Contains(term, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public static string UpdatePerson(Person person, Person updated) 
+        {
+            return new Func<String>(() => {
+                if (CheckContactExistence(person))
                 {
-                    message = "Person already exists.";
-                    ok = true;
-                    break;
+                    people.Remove(person);
+                    people.Add(updated);
+                    return $"Contact updated successfully.\nOld data:\n {person.FirstName} {person.SurnameName} | Birthday: {person.Birthday}" +
+                           $"\nNew data:\n {updated.FirstName} {updated.SurnameName} | Birthday: {updated.Birthday}";
                 }
-            }
+                else
+                {
+                    return "Person doesn't exists.";
+                }
+            })();
+        }
+        public static string DeletePerson(Person person) 
+        {
+            return new Func<String>(() => {
+                if (CheckContactExistence(person)) {
+                    people.Remove(person);
+                    return $"{person.FirstName} {person.SurnameName} successfully deleted.";
+                }
+                else 
+                { 
+                    return "Person added."; 
+                }
+            })();
+        }
 
-            if (ok == false) 
-            {
-                people.Add(person);  
-                message = "Person added.";
-            }
-
-            return message;
-            
-        }
-        public static string searchPeople() 
+        private static Boolean CheckContactExistence(Person person) 
         {
-            return "";
-        }
-        public static string updatePerson() 
-        {
-            return "";
-        }
-        public static string deletePerson() 
-        {
-            return "";
+            return new Func<Boolean>(() => {
+                var ok = false;
+                foreach (var match in people)
+                {
+                    if (match.FirstName.Equals(person.FirstName)
+                        && match.SurnameName.Equals(person.SurnameName)
+                        && match.Birthday.Equals(person.Birthday))
+                    {
+                        ok = true;
+                        break;
+                    }
+                }
+                return ok;
+            })();
         }
     }
 }
